3da59e89ad30c12e61bdfe1536f23421d0e25804
karsv <karsv.dvz@gmail.com>
2020-02-04 12:40:04 +0200

    add Injector, and mark interfaces (#1)
    * add Injector, and mark interfaces
    create new model Movie
    add DAO and Service for Movie
    configure Hibernate
    
    * add empty line at and of file

---
+++ src/main/java/com/dev/cinema/dao/impl/MovieDaoImpl.java
@@ -0,0 +1,52 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.MovieDao;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.Movie;
+import com.dev.cinema.util.HibernateUtil;
+
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaQuery;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    private static final Logger LOGGER = LogManager.getLogger(MovieDaoImpl.class);
+
+    @Override
+    public Movie add(Movie movie) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Long itemId = (Long) session.save(movie);
+            transaction.commit();
+            movie.setId(itemId);
+            return movie;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            LOGGER.error("Can't insert Movie entity", e);
+            throw new RuntimeException("Can't insert Movie entity", e);
+        }
+    }
+
+    @Override
+    public List<Movie> getAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CriteriaQuery<Movie> criteriaQuery = session.getCriteriaBuilder()
+                    .createQuery(Movie.class);
+            criteriaQuery.from(Movie.class);
+            return session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception e) {
+            LOGGER.error("Error when getting all movies", e);
+            throw new RuntimeException("Error when getting all movies", e);
+        }
+    }
+}
