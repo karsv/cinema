15e14cea8b8de4aa75d90fa853fa23615b9e5ffd
karsv <Pbvf2019!>
2020-02-05 11:04:47 +0200

    delete unused imports

--- src/main/java/com/dev/cinema/dao/impl/MovieDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/MovieDaoImpl.java
@@ -9,9 +9,6 @@ import java.util.List;
 
 import javax.persistence.criteria.CriteriaQuery;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
