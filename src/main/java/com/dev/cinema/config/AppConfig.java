9f10fc5019321db55fde00f3538d15e99ca809be
karsv <Pbvf2019!>
2020-02-25 10:42:34 +0200

    del unused imports

--- src/main/java/com/dev/cinema/config/AppConfig.java
+++ src/main/java/com/dev/cinema/config/AppConfig.java
@@ -1,14 +1,5 @@
 package com.dev.cinema.config;
 
-import com.dev.cinema.model.CinemaHall;
-import com.dev.cinema.model.Movie;
-import com.dev.cinema.model.MovieSession;
-import com.dev.cinema.model.Order;
-import com.dev.cinema.model.Role;
-import com.dev.cinema.model.ShoppingCart;
-import com.dev.cinema.model.Ticket;
-import com.dev.cinema.model.User;
-
 import java.util.Properties;
 
 import javax.sql.DataSource;
